6d398816d4ce054c9d62b8b590c3765f865f52be
JohanSannier <johan.sanniercaffier@gmail.com>
2022-05-16 15:28:18 +0200

    First commit

---
+++ src/components/TagName.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/TagName.css";
+
+function TagName(props) {
+  return (
+    <div>
+      <span className="tag">Test</span>
+    </div>
+  );
+}
+
+export default TagName;
