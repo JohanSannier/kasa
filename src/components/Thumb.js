197744f33cbd8d3a8825e7453f3a7ed7062df517
JohanSannier <johan.sanniercaffier@gmail.com>
2022-05-21 12:14:08 +0200

    Home page

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../styles/Thumb.css";
 
-function Thumb(props) {
+function Thumb({ id, cover, title }) {
   return (
-    <div className="thumb">
-      <img src="#" alt="#" />
-      <p className="thumb-title">Titre de la location</p>
+    <div className="thumb" id={id}>
+      <img src={cover} alt={title} className="thumb-img" />
+      <p className="thumb-title">{title}</p>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,25 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import Thumb from "../components/Thumb";
 import background from "../assets/bg-home.png";
+import data from "../data/data.json";
 
 function Home(props) {
   return (
     <>
       <Header />
       <Hero background={background} text="Chez vous, partout et ailleurs" />
+      <section className="location-display">
+        {data.map((card) => (
+          <Thumb
+            key={card.id}
+            id={card.id}
+            cover={card.cover}
+            title={card.title}
+          />
+        ))}
+      </section>
       <Footer />
     </>
   );
