47eadfb0808c446211e7d36af5c0ed94b8cb6f62
JohanSannier <johan.sanniercaffier@gmail.com>
2022-05-24 10:29:30 +0200

    Dropdown ok

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,33 +4,39 @@ import arrow_up from "../assets/arrow_up.png";
 import arrow_down from "../assets/arrow_down.png";
 
 function Dropdown({ title, content }) {
-  const [arrow, setArrow] = useState(arrow_up);
+  // Rendu conditionnel sur l'affichage de l'icone
   const [isOpen, setIsOpen] = useState(true);
-  // console.log(content);
+  console.log(content);
   // console.log(isOpen);
   let dropdown = document.querySelector(".dropdown-list");
-  let typeOfContent = typeof content;
+  // let typeOfContent = typeof content;
+  const [typeOfContent, setTypeOfContent] = useState(typeof content);
 
+  console.log(typeOfContent);
   function handleClick() {
     setIsOpen(!isOpen);
-    console.log("====================================");
-    console.log(isOpen);
-    console.log("====================================");
-    setArrow(isOpen ? arrow_up : arrow_down);
   }
 
   return (
     <div className="dropdown-wrapper">
       <p className="dropdown-title">{title}</p>
-      <img src={arrow} alt="" className="arrow-icon" onClick={handleClick} />
-      <ul className="dropdown-list">
-        {content}
-        {/* {typeOfContent === "object"
-          ? content.map((list, index) => {
+      <img
+        src={isOpen ? arrow_up : arrow_down}
+        alt=""
+        className="arrow-icon"
+        onClick={handleClick}
+      />
+      {isOpen && (
+        <ul className="dropdown-list">
+          {typeOfContent === "string" ? (
+            <li>{content}</li>
+          ) : (
+            content.map((list, index) => {
               return <li key={`item ${index}`}>{list}</li>;
             })
-          : { content }} */}
-      </ul>
+          )}
+        </ul>
+      )}
     </div>
   );
 }
